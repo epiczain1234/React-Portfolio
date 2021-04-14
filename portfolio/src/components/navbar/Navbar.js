1f713e852d06be9ef6f1037cc21e037c0dca85dc
epiczain1234 <mukatizain@gmail.com>
2021-04-14 14:52:42 -0400

    Added styling to menu

--- portfolio/src/components/navbar/Navbar.js
+++ portfolio/src/components/navbar/Navbar.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react'
 import {MenuItems} from "./Menuitems"
+import './Navbar.css'
 class Navbar extends Component{
+    state = {clicked:false}
+
+
     render(){
         return(
             <nav className="NavbarItems"> 
-                <h1 className="nabvar-logo">React<i className="fab fa-react"></
-                i></h1>
-                <div className="menu-icon">
+                <h1 className="nabvar-logo">React<i className="fab fa-react"></i></h1>
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
 
                 </div>
                 <ul>
                     {MenuItems.map((item, index) => {
                         return(
                             <li key={index}>
-                                <a className={item.cName} href={item.url}></a>
+                                <a className={item.cName} href={item.url}>
                                 {item.title}
+                                </a>
                             </li>
                         )
                     })}
